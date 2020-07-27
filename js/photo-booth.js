4e1a54e01d799203317d31757dd1f63370e4d9c1
Carlos Teller <carlos.z.teller@gmail.com>
2020-07-27 15:13:06 -0500

    Added functionality to clear the photos from the canvas to reset photo strip

--- js/photo-booth.js
+++ js/photo-booth.js
@@ -7,6 +7,7 @@ const clearPhotosButton = document.getElementById('clear-photos');
 const downloadPhotoStripButton = document.getElementById('download-photo-strip');
 const ctx = canvas.getContext('2d');
 
+// Initialize webcam, set video source to webcam
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: false
@@ -17,6 +18,7 @@ navigator.mediaDevices.getUserMedia({
     console.log(`Error: ${err}`);
 });
 
+// Set webcam and canvas sizes, draw "photo stip"
 webcam.addEventListener('canplay', () => {
     webcam.setAttribute('width', photoBoothContainer.offsetWidth);
     webcam.setAttribute('height', webcam.videoHeight / (webcam.videoWidth / 500));
@@ -27,18 +29,22 @@ webcam.addEventListener('canplay', () => {
     ctx.rect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'white';
     ctx.strokeStyle = 'black';
-    ctx.lineWidth = 5;
+    ctx.lineWidth = 2;
     ctx.fill();
     ctx.stroke();
 });
 
+// Event listener for when someone
 takePhotoButton.addEventListener('click', (e) => {
     e.preventDefault();
+
+    takePhotoButton.style.display = 'none';
 
     photoTimer();
     takePhotos();
 });
 
+// Timer - countdown from 3 seconds to "Say Cheese!"
 photoTimer = () => {
     let seconds = 4;
 
@@ -60,6 +66,7 @@ photoTimer = () => {
     }, 1000);
 }
 
+// Take 4 photos and add them to the canvas
 takePhotos = () => {
     let photoCount = 1;
 
@@ -87,16 +94,38 @@ takePhotos = () => {
         if(photoCount <= 4) {
             photoTimer();
         }
+
+        if(photoCount > 4) {
+            clearPhotosButton.style.display = 'block';
+            downloadPhotoStripButton.style.display = 'block';
+        }
     }, 5000);
 }
 
+// Clear all photos from the canvas
+clearPhotosButton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'white';
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 2;
+    ctx.fill();
+    ctx.stroke();
+
+    takePhotoButton.style.display = 'block';
+    clearPhotosButton.style.display = 'none';
+    downloadPhotoStripButton.style.display = 'none';
+});
+
+// Download the "photo strip" -- a souvenir of sorts
 downloadPhotoStripButton.addEventListener('click', (e) => {
     e.preventDefault();
 
     const a = document.createElement('a');
     const imgURL = canvas.toDataURL('image/jpg');
     
-    a.download = 'my-photo-strip.jpg';
+    a.download = 'mintbean-mirrorhack-photo-strip.jpg';
     a.href = imgURL;
     a.click();
 })
